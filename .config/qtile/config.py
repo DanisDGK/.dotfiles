77b55ed7de02517ac395819599347e32487c1e38
DanisDGK <danisdgk@gmail.com>
2021-01-27 21:23:02 +0200

    Added group for drawing to Qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -117,7 +117,8 @@ group_names=[("  ",{'layout': def_layout, 'spawn':'kitty'}),
            ("  ",{'layout': def_layout, 'spawn':'kitty -e zsh -c ua-update-all'}), 
            ("  ",{'layout': def_layout, 'spawn':'kitty -e /home/daniel/mic_over_mumble/mic_over_mumble'}),
            (" ﭮ ",{'layout': def_layout, 'spawn':'discord'}),
-           ("  ",{'layout': def_layout, 'spawn':'kitty -e /home/daniel/.local/bin/yterm'})]
+           ("  ",{'layout': def_layout, 'spawn':'kitty -e /home/daniel/.local/bin/yterm'}),
+           ("  ",{'layout': 'floating'})]
 
 groups = [Group(name, **kwargs) for name, kwargs in group_names]
 
